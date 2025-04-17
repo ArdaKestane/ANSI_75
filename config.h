79fe1fa753f26b481afb34d8513d9a1f9da16760
Arda Kestane <arda.kestane@digitus.com.tr>
2025-04-17 22:24:42 +0300

    Initial commit: ANSI 75% keyboard files

---
+++ config.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define MATRIX_ROWS 6
+#define MATRIX_COLS 16
+
+#define MATRIX_ROW_PINS { 25, 24, 23, 22, 21, 20 }
+#define MATRIX_COL_PINS { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 }
+
+#define DIODE_DIRECTION COL2ROW
